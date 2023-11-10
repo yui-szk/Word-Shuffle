a2a570cb4ea885bd1c75ed19d5f03413f227d606
yui-szk <yuisuzuki.017@gmail.com>
2023-11-10 21:45:36 +0900

    setに置き換え

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
 const shuffleBtn = document.getElementById("shuffle-button");
 const result = document.getElementById("result-box");
 
-shuffleBtn.addEventListener("click", () => {
-  const textElm = document.getElementById("text-box");
-  const inputText = textElm.value;
-  const arrayInputText = [...inputText];
-  arrayInputText.sort((a, b) => Math.random() - 0.5);
-  result.innerHTML = arrayInputText.join("");
-});
-
-function enter() {
+function set() {
   const textElm = document.getElementById("text-box");
   const inputText = textElm.value;
   const arrayInputText = [...inputText];
   arrayInputText.sort((a, b) => Math.random() - 0.5);
   result.innerHTML = arrayInputText.join("");
 }
+
+shuffleBtn.addEventListener("click", () => {
+  set();
+});
