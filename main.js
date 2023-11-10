c2a55e4e5a0f714dd1565a1405c6efb81c7862d3
yui-szk <yuisuzuki.017@gmail.com>
2023-11-11 01:52:07 +0900

    added explanation

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 const shuffleBtn = document.getElementById("shuffle-button");
 const result = document.getElementById("result-box");
 
-function set() {
-  const textElm = document.getElementById("text-box");
-  const inputText = textElm.value;
-  const arrayInputText = [...inputText];
-  arrayInputText.sort((a, b) => Math.random() - 0.5);
-  result.innerHTML = arrayInputText.join("");
+function shuffleResult() {
+  const inputText = document.getElementById("text-box").value; // テキストボックスのテキストを取得
+  const arrayInputText = [...inputText]; // 取得したテキストを1文字ごとの配列に変更
+  arrayInputText.sort((a, b) => Math.random() - 0.5); // 配列の各要素に乱数を与え、sort
+  result.innerHTML = arrayInputText.join(""); // 配列を文字列に戻し、htmlに表示
 }
 
+// ボタンをクリックしたときにshuffleResultを実行
 shuffleBtn.addEventListener("click", () => {
-  set();
+  shuffleResult();
 });
