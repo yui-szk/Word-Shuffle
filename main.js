79c86df7961149db5a025e87c2bde28ed9feaeb0
yui-szk <yuisuzuki.017@gmail.com>
2023-11-13 01:18:05 +0900

    update main.js

--- main.js
+++ main.js
@@ -2,17 +2,14 @@ const shuffleBtn = document.getElementById("shuffle-button");
 const result = document.getElementById("result-box");
 
 function shuffleResult() {
-  // テキストボックスのテキストを取得
   const inputText = document.getElementById("text-box").value;
   // 取得したテキストを1文字ごとの配列に変更
   const arrayInputText = [...inputText];
   // 配列の各要素に乱数を与え、sort
   arrayInputText.sort((a, b) => Math.random() - 0.5);
-  // 配列を文字列に戻し、htmlに表示
   result.innerHTML = arrayInputText.join("");
 }
 
-// ボタンをクリックしたときにshuffleResultを実行
 shuffleBtn.addEventListener("click", () => {
   shuffleResult();
 });
